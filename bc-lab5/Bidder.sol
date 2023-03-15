08d08a07df8cf5d7ad34817ffed5922bf6aedac5
Wickey Chai <wickeychai02@gmail.com>
2023-03-15 11:51:05 +0800

    bidder

---
+++ bc-lab5/Bidder.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Bidder {
+    string name;
+    uint bidAmount;
+    bool eligible;
+    uint minBid;
+
+    function setName() public {
+
+    }
+
+    function setBidAmount() public {
+        
+    }
+
+    function determineEligibility() public {
+        
+    }
+}
+
